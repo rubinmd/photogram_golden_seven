465be3cf742bff90977608bd1102a50396496bfc
rubinmd <matt@Matthews-MacBook-Pro.local>
2015-10-25 14:42:31 -0500

    Done
    Finished it up. Should have committed more.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,5 +7,35 @@ class PhotosController < ApplicationController
     @id=params[:id]
   end
 
+  def new_form
+  end
 
+  def create_row
+    p = Photo.new
+    p.source = params["the_source"]
+    p.caption = params["the_caption"]
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    @id=params[:id]
+    Photo.find(@id).destroy
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def edit_form
+    @id=params[:id]
+  end
+
+  def update_row
+    @id=params[:id]
+    @caption=params["the_caption"]
+    @source=params["the_source"]
+    p=Photo.find(@id)
+    p.caption=@caption
+    p.source=@source
+    p.save
+    redirect_to("http://localhost:3000/photos")
+  end
 end
